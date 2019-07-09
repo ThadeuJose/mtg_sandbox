9a2592242889078281386d41ed03ca3a83419789
Thadeu José <thadeujs@gmail.com>
2019-07-09 00:34:08 -0300

    Drag and Drop from Hand works
    Use jquery Draggable to be capable of get a card and drag and drop to main area

--- README.md
+++ README.md
@@ -2,20 +2,45 @@
 Two deck sandbox to Magic The Gathering 
 
 # Next Step:
-Import the two deck list in textformat(Remenber to put a default)  
-	Show a text area 
-	Hide text area when submit 
-	Put a import toggle 
-	Use the good citizen policies - https://scryfall.com/docs/api
-
+Hand Issue 
+	When a card is stop(https://api.jqueryui.com/draggable/#event-stop) 	
+		All card shift to that position 
+		https://stackoverflow.com/questions/33507237/jquery-draggable-append-to-same-position
 
 # TODO
-Make the 7 starter draw and draw when click on library
 
-drag and drop from hand to main area 
+Make the 7 card draw 
+	Make a button in the status bar 	
+	
+Make the london mulligan 
+	Only say how many card need to go 
 
+Fix the grid 	
+	Make the status bar  
+	Fix the column of the deck 
+	Put the exile text and box 
+		
+Put the name and cmc in the card 
 
+Drop into graveyard 
+	https://stackoverflow.com/questions/9796934/jquery-ui-remove-element-when-dropped-into-a-div-using-droppable
+
+Zoom card on hover for now 
+	https://api.jquery.com/hover/
+	
+Graveyard and exile 
+	Droppable
+
+Import the two deck list in textformat(Remenber to put a default)  
+	Make the other person 
+	Put a import toggle 	
+	Put a symbol of up and down
+	Deck can only have 60 cards and cant be empty 		
+	Play area only appears after the two decks be imported
+	
 # Technical Debt
+
+do the error messages
 
 Use sass
 
--- js/main.js
+++ js/main.js
@@ -16,31 +16,55 @@ $(function () {
      decklist_player_2.push(...new_cards);
      console.log(new_cards);
      console.log(decklist_player_2);
-
    }
+   shuffleArray(decklist_player_2)
+   console.log(decklist_player_2);
+   $('#decklist_down').hide();
   });
 
-  $('#library').click(function() {
-    console.log('Library');
+  $('#library_player_2').click(function() {
+    let cardname = decklist_player_2.shift();
+    createCard(cardname).then(img => {
+      img.appendTo($("#hand_player_2"));
+    });
   })
 });
 
+const createCard = (cardname) => {
+  return new Promise((resolve, reject) => {
+    $.get( 'https://api.scryfall.com/cards/named?exact='+cardname, function(data){
+      let imageUrl = data.image_uris.normal;
+      if (imageUrl) {
+        let cardBox = $("<div class='card_box'>").draggable();
+        let img = $('<img class="card">');
+        img.attr('src', data.image_uris.normal);
+        img.appendTo(cardBox);
+        console.log(data.image_uris.normal);
+        sleep(50);
+        resolve(cardBox);
+      } else {
+        // TODO: Error message
+        reject(Error("It broke"));
+      }
+    });
+  });
+};
 
-// $("#submitDecklistDown").click(function() {
-//   let text = $('#decklistDown').val();
-//   let lines = text.split(/\r?\n/);
-//   let decklist = [];
-//   for (l of lines) {
-//     row = l.substr(0, l.indexOf('(')-1);
-//     decklist.push(row);
-//   }
-//   cardname = decklist[3];
-//   cardname = cardname.substr(cardname.indexOf(' '));
-//
-//   $.get( 'https://api.scryfall.com/cards/named?exact='+cardname, function(data){
-//     var img = $('<img class="hand_img">');
-//     img.attr('src', data.image_uris.normal);
-//     img.appendTo('#hand');
-//     console.log(data.image_uris.normal);
-//   });
-// });
+
+
+/**
+ * Randomize array element order in-place.
+ * Using Durstenfeld shuffle algorithm.
+ */
+function shuffleArray(array) {
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+}
+// Use await sleep(ms); to use
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
